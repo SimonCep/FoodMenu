4cae6dc6ee163e12778a6fcd65477a74b5d1b412
Simciks <simas.ceponis@gmail.com>
2023-03-24 14:58:56 +0200

    Added functionality.

--- kulinarai/src/App.js
+++ kulinarai/src/App.js
@@ -8,9 +8,8 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-      <main>
-        <SearchBar />
-      </main>
+      <Sidebar />
+      <SearchBar />
     </div>
   )
 };
