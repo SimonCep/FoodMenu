16a3efde13271da28d7f7694d6cc09c44e30bcd6
Simciks <simas.ceponis@gmail.com>
2023-03-26 22:57:12 +0300

    Fixed the sidebar to be opened normally.

--- kulinarai/src/App.js
+++ kulinarai/src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './App.css'
 import SearchBar from './components/SearchBar/searchBar'
 import Navbar from './components/NavBar/navBar';
-import Sidebar from './components/SideBar/sideBar';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Sidebar />
       <SearchBar />
     </div>
   )
--- kulinarai/src/components/NavBar/navBar.js
+++ kulinarai/src/components/NavBar/navBar.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Button, Offcanvas } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 import './navBar.css';
 
 function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const handleToggle = () => setShowSidebar(!showSidebar);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="mx-auto">
-        <a className="navbar-brand" href="/" style={{ fontSize: '40px' }}>Kulinarai</a>
-      </div>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-    </nav>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Button variant="primary" onClick={handleToggle}>
+          <span className="visually-hidden">Toggle navigation</span>
+          <span className="navbar-toggler-icon" />
+        </Button>
+        <div className="mx-auto">
+          <a className="navbar-brand" href="/" style={{ fontSize: '40px' }}>Kulinarai</a>
+        </div>
+      </nav>
+
+      <Offcanvas show={showSidebar} onHide={() => setShowSidebar(false)}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title className="sideHeader">Kulinarai. Receptai jums.</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ul className="list-sidebar">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+          </ul>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
   );
 }
 
