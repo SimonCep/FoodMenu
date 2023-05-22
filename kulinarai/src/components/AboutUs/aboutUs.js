4a179a8c409ff1196c35849bb119e61b86c5e0f5
SimonCep <103537480+SimonCep@users.noreply.github.com>
2023-05-22 18:34:56 +0300

    Fixed bugs and migrated to github.

--- kulinarai/src/components/AboutUs/aboutUs.js
+++ kulinarai/src/components/AboutUs/aboutUs.js
@@ -7,29 +7,15 @@ function AboutUs() {
     <Container className='AboutUs'>
       <h1 className='about-header'>About Us</h1>
       <p className='for-paragraph'>
-        The website was created by the students of Kaunas University of Technology.
-        They are all studying in informatics engineering and belong to IFIN-1/3.
-        Their names are: Simas Čeponis, Justas Bujko, Tadas Lekerauskas. This website was created using
-        agile methodology. Every member of the team has a few roles. Here is the information about us:
+        The website was created by a student of Kaunas University of Technology.
+        Here is some information about me:
       </p>
       <Row>
-        <Col className='person' md={4}>
+        <Col className='person' md={4} >
           <Image className='profile-pic' src={require('../../images/Simas.jpg')} />
           <h3 className='person-name'>Simas Čeponis</h3>
-          <p>Roles: programmer, team leader</p>
-          <p>E-Mail: simas.ceponis@ktu.edu</p>
-        </Col>
-        <Col className='person' md={4}>
-          <Image className='profile-pic' src={require('../../images/Justas.png')} />
-          <h3 className='person-name'>Justas Bujko</h3>
-          <p>Roles: programmer, product leader</p>
-          <p>E-Mail: justas.bujko@ktu.edu</p>
-        </Col>
-        <Col className='person' md={4}>
-          <Image className='profile-pic' src={require('../../images/Tadas.jpg')} />
-          <h3 className='person-name'>Tadas Lekerauskas</h3>
-          <p>Roles: programmer, designer</p>
-          <p>E-Mail: tadas.lekerauskas@ktu.edu</p>
+          <p>Roles: Software engineerer</p>
+          <p>E-Mail: simas.ceponis@gmail.com</p>
         </Col>
       </Row>
     </Container>
